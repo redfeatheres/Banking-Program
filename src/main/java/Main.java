db91bda5c4cf921bc310fcde5510a80905dd76a6
Andrey-Krasnoperov <krasnoperov.andrey.a@gmail.com>
2026-09-07 10:32:17 +0300

    Create an entry point - Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // DECLARE VARIABLES
+        Scanner scanner = new Scanner(System.in);
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
+        System.out.println("_______________");
+        System.out.println("BANKING PROGRAM");
+        System.out.println("_______________");
+        System.out.println("1. Show balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+
+        System.out.println("CHOOSE AN OPTION: ");
+        choice = scanner.nextInt();
+        while (choice != 4) {
+            
+        }
+
+        // DISPLAY MENU
+
+        // GET AND PROCESS USERS CHOICE
+
+        // showBalance
+
+        // deposit()
+
+        // withdraw()
+
+        // EXIT MSG
+    }
+}
